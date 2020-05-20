69c7af3a06e1ff2eb130a660003c99e57d6abfc2
sergey <tsn3ps@gmail.com>
2020-05-21 00:58:22 +0300

    first commit

---
+++ value-of-playcards.php
@@ -0,0 +1,20 @@
+﻿
+
+<?php
+$x='валет';
+$list=array(
+    7=>7,
+    8=>8,
+    9=>9,
+    10=>10,
+    11=>"валет",
+    12=>"дама",
+    13=>"король",
+    14=>"туз");
+foreach ($list as $key=> $value){
+    if ($x==$list[$key])
+        echo "playing cards\"$x\"- value $key.";
+}
+?>
+
+
